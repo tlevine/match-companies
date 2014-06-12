238cebb4cb7e6c06ea773f204fd5c615272af9d0
Thomas Levine <_@thomaslevine.com>
2014-06-12 17:55:32 -0400

    reconcile.py

---
+++ reconcile.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import re
+import itertools
+from functools import partial
+import os, csv, sys
+
+from thready import threaded
+from company import reconcile
+
+def ask(writer, args):
+    contract_uri, query = args
+    if query != '':
+        results = reconcile(None, query)
+        if len(results) > 0:
+            name = results[0]['name']
+            uri = results[0]['uri']
+        else:
+            name = uri = None
+        writer.writerow({
+            'contract.uri': contract_uri,
+            'original.company.name': query,
+            'opencorporates.company.name': name,
+            'opencorporates.company.uri': uri,
+        })
+
+def strip(x:str) -> str:
+    return x.strip(' \r\n,-;:()#')
+def split(company_name:str) -> list:
+    '''
+    Split up a company name into one or more real company names.
+    '''
+    _split = r'(?:in association with|[^a-z]and | JV |Leader|Partner| y )'
+    _remove = r'(?:(?:^JV | JV )(?:of )?| JV$|\(?Lead(?:ing)? partner\)?|Consortium of:? ?|Leading|Partner|^JV:? ?|Joint venture(?: of)?|^M\/?S |\([^\)]+\))'
+    if re.search(r'(?:in association with|Consortium of|JV |^JV|joint venture| y )', company_name, flags = re.IGNORECASE):
+        messy = re.split(_split, company_name, flags = re.IGNORECASE)
+        return list(filter(None, map(lambda x: strip(re.sub(_remove, '', x, flags = re.IGNORECASE)), messy)))
+    else:
+        return [company_name]
+
+def respell(company_name:str) -> str:
+    n = str(company_name)
+    n = re.sub(r'^Ltd ?(.*)$', r'\1 Ltd', n, flags = re.IGNORECASE)
+ #  n += ')' * (n.count('(') - n.count(')')) # Balance parantheses
+    n = re.sub(r'\([^)]*$', '', n)
+    n = strip(re.sub(r'(?:M/[Ss]|«|»)', '', n))
+    return n
+
+def args(reader):
+    for company in reader:
+        for name in split(company['Name']):
+            yield company['Link'], name
+
+def main():
+    reader = csv.DictReader(open(os.path.join('pagedata', 'company.csv')))
+    writer = csv.DictWriter(sys.stdout, fieldnames = ['contract.uri', 'original.company.name', 'opencorporates.company.name', 'opencorporates.company.uri'])
+    writer.writeheader()
+    threaded(args(reader), partial(ask, writer), num_threads = 30)
+
+if __name__ == '__main__':
+    main()
