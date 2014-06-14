4f3b164aa491d808cb8e17cf10222a2109043605
Thomas Levine <_@thomaslevine.com>
2014-06-14 09:29:12 -0400

    colnames

--- reconcile.py
+++ reconcile.py
@@ -12,6 +12,29 @@ from company import reconcile
 # COUNTRY = '(%s)' % re.sub(r' ?[\(\)] ?', '|', '|'.join(row['Name'] for row in DataPackage('http://data.okfn.org/data/core/country-list/').data)).replace('||','|')
 COUNTRY = re.compile(r"^(?:.*[^a-z])?(Afghanistan|Åland Islands|Albania|Algeria|American Samoa|Andorra|Angola|Anguilla|Antarctica|Antigua and Barbuda|Argentina|Armenia|Aruba|Australia|Austria|Azerbaijan|Bahamas|Bahrain|Bangladesh|Barbados|Belarus|Belgium|Belize|Benin|Bermuda|Bhutan|Bolivia, Plurinational State of|Bonaire, Sint Eustatius and Saba|Bosnia and Herzegovina|Botswana|Bouvet Island|Brazil|British Indian Ocean Territory|Brunei Darussalam|Bulgaria|Burkina Faso|Burundi|Cambodia|Cameroon|Canada|Cape Verde|Cayman Islands|Central African Republic|Chad|Chile|China|Christmas Island|Cocos|Keeling|Islands|Colombia|Comoros|Congo|Congo, the Democratic Republic of the|Cook Islands|Costa Rica|Côte d'Ivoire|Croatia|Cuba|Curaçao|Cyprus|Czech Republic|Denmark|Djibouti|Dominica|Dominican Republic|Ecuador|Egypt|El Salvador|Equatorial Guinea|Eritrea|Estonia|Ethiopia|Falkland Islands|Malvinas|Faroe Islands|Fiji|Finland|France|French Guiana|French Polynesia|French Southern Territories|Gabon|Gambia|Georgia|Germany|Ghana|Gibraltar|Greece|Greenland|Grenada|Guadeloupe|Guam|Guatemala|Guernsey|Guinea|Guinea-Bissau|Guyana|Haiti|Heard Island and McDonald Mcdonald Islands|Holy See|Vatican City State|Honduras|Hong Kong|Hungary|Iceland|India|Indonesia|Iran, Islamic Republic of|Iraq|Ireland|Isle of Man|Israel|Italy|Jamaica|Japan|Jersey|Jordan|Kazakhstan|Kenya|Kiribati|Korea, Democratic People's Republic of|Korea, Republic of|Kuwait|Kyrgyzstan|Lao People's Democratic Republic|Latvia|Lebanon|Lesotho|Liberia|Libya|Liechtenstein|Lithuania|Luxembourg|Macao|Macedonia, the Former Yugoslav Republic of|Madagascar|Malawi|Malaysia|Maldives|Mali|Malta|Marshall Islands|Martinique|Mauritania|Mauritius|Mayotte|Mexico|Micronesia, Federated States of|Moldova, Republic of|Monaco|Mongolia|Montenegro|Montserrat|Morocco|Mozambique|Myanmar|Namibia|Nauru|Nepal|Netherlands|New Caledonia|New Zealand|Nicaragua|Niger|Nigeria|Niue|Norfolk Island|Northern Mariana Islands|Norway|Oman|Pakistan|Palau|Palestine, State of|Panama|Papua New Guinea|Paraguay|Peru|Philippines|Pitcairn|Poland|Portugal|Puerto Rico|Qatar|Réunion|Romania|Russian Federation|Rwanda|Saint Barthélemy|Saint Helena, Ascension and Tristan da Cunha|Saint Kitts and Nevis|Saint Lucia|Saint Martin|French part|Saint Pierre and Miquelon|Saint Vincent and the Grenadines|Samoa|San Marino|Sao Tome and Principe|Saudi Arabia|Senegal|Serbia|Seychelles|Sierra Leone|Singapore|Sint Maarten|Dutch part|Slovakia|Slovenia|Solomon Islands|Somalia|South Africa|South Georgia and the South Sandwich Islands|South Sudan|Spain|Sri Lanka|Sudan|Suriname|Svalbard and Jan Mayen|Swaziland|Sweden|Switzerland|Syrian Arab Republic|Taiwan, Province of China|Tajikistan|Tanzania, United Republic of|Thailand|Timor-Leste|Togo|Tokelau|Tonga|Trinidad and Tobago|Tunisia|Turkey|Turkmenistan|Turks and Caicos Islands|Tuvalu|Uganda|Ukraine|United Arab Emirates|United Kingdom|United States|United States Minor Outlying Islands|Uruguay|Uzbekistan|Vanuatu|Venezuela, Bolivarian Republic of|Viet Nam|Virgin Islands, British|Virgin Islands, U.S.|Wallis and Futuna|Western Sahara|Yemen|Zambia|Zimbabwe)", flags = re.IGNORECASE)
 
+MATCHNAMES = [
+    (r'^address$', 'address'),
+    (r'^name$', 'name'),
+    ('opening.*price', 'price.opening'),
+    ('price.*opening', 'price.opening'),
+    ('evaluat(?:ed|ion).*price', 'price.evaluated'),
+    ('country', 'country'),
+    (r'reason.*rejection', 'reason.rejection'),
+    (r'contract ?price', 'price.contract'),
+    ('duration', 'duration'),
+    (r'method.*procurement', 'procurement.method'),
+    ('scope', 'scope'),
+    (r'signature.*date', 'date.signature'),
+    (r'technical.*score', 'score.technical'),
+    (r'financial.*score', 'score.financial'),
+    (r'final.*score', 'score.final'),
+    (r'final ?ranking', 'ranking.final'),
+    (r'small ?contract', 'small.contract.notice'),
+    (r'method.*procurement', 'method.procurement'),
+    (r'method.*selection', 'method.selection'),
+    (r'selection.*method', 'method.selection'),
+]
+
 def profile():
     raw_bids = defaultdict(lambda: [])
     for row in csv.DictReader(open(os.path.join('pagedata','company-profile.csv'))):
@@ -19,28 +42,7 @@ def profile():
 
     for contractid, raw_bid in raw_bids.items():
         bid = {'contract.id':contractid}
-        for match, name in [
-                (r'^address$', 'address'),
-                (r'^name$', 'name'),
-                ('opening.*price', 'price.opening'),
-                ('price.*opening', 'price.opening'),
-                ('evaluat(?:ed|ion).*price', 'price.evaluated'),
-                ('country', 'country'),
-                (r'reason.*rejection', 'reason.rejection'),
-                (r'contract ?price', 'price.contract'),
-                ('duration', 'duration'),
-                (r'method.*procurement', 'procurement.method'),
-                ('scope', 'scope'),
-                (r'signature.*date', 'date.signature'),
-                (r'technical.*score', 'score.technical'),
-                (r'financial.*score', 'score.financial'),
-                (r'final.*score', 'score.final'),
-                (r'final ?ranking', 'ranking.final'),
-                (r'small ?contract', 'small.contract.notice'),
-                (r'method.*procurement', 'method.procurement'),
-                (r'method.*selection', 'method.selection'),
-                (r'selection.*method', 'method.selection'),
-            ]:
+        for match, name in MATCHNAMES:
             for key, value in raw_bid:
                 if re.search(match, key, flags = re.IGNORECASE):
                     bid[name] = value
@@ -128,7 +130,7 @@ def main():
         'original.company.country',
         'opencorporates.company.name',
         'opencorporates.company.uri',
-    ]
+    ] + list(set(x for _, x in MATCHNAMES))
     reader = csv.DictReader(open(os.path.join('pagedata', 'company.csv')))
     writer = csv.DictWriter(sys.stdout, fieldnames = fieldnames)
     writer.writeheader()
